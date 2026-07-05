dd6dbf208af8355ce1d9c3fc5234e490607d810e
farhan <farhan.azizi.gh@gmail.com>
2026-07-05 16:45:18 +0330

    I created User model for users.
    And use this command in terminal: 'python manage.py makemigrations' & 'python manage.py migrate'

--- account/models.py
+++ account/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.crypto import get_random_string
 
-# Create your models here.
+
+
+class User(AbstractUser):
+    address = models.TextField(null=True, blank=True, verbose_name='آدرس')
+    avatar = models.ImageField(upload_to='users/avatar', null=True, blank=True, verbose_name='آواتار')
+    phone = models.CharField(max_length=11, null=True, blank=True, verbose_name='شماره تلفن')
+    email_active_code = models.CharField(max_length=128, null=False, blank=False, default=get_random_string(128), verbose_name='کد فعالسازی حساب کاربری')
+    
+    
+    def __str__(self):
+        return f'{self.username} / {self.email}'
+    
+    
+    class Meta:
+        db_table = 'account'
+        db_table_comment = 'User information is located in this table.'
+        ordering = ['is_active']
