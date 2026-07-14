d1900fafc472e1d751060132119659dec2898f90
farhan <farhan.azizi.gh@gmail.com>
2026-07-14 16:42:31 +0330

    I created Category model.
    And use command in terminal "python manage.py makemigrations" and "python manage.py migrate". And created admin for Category model

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Category
 
-# Register your models here.
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "url_name",
+        "is_active",
+        "updated_at",
+        "created_at",
+    )
+    list_filter = (
+        "created_at",
+        "updated_at",
+        "is_active",
+    )
+    list_editable = ("is_active",)
+    readonly_fields = (
+        "updated_at",
+        "created_at",
+    )
+    date_hierarchy = "created_at"
+    search_fields = (
+        "name",
+        "url_name",
+    )
--- product/models.py
+++ product/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from basic.base_model import BaseModel
 
-# Create your models here.
+
+
+class Category(BaseModel):
+    name = models.CharField(max_length=120, null=False, blank=False, verbose_name='نام دسته بندی')
+    url_name = models.SlugField(max_length=120, null=False, blank=False, verbose_name='آدرس دسته بندی در url')
+    parent = models.ForeignKey(to='Category', on_delete=models.SET_NULL, null=True, blank=True, verbose_name='دسته بندی والد')
+    image = models.ImageField(upload_to='category/image', null=True, blank=True, verbose_name='عکس دسته بندی', help_text='فقط برای دسته بندی های والد استفاده می شود')
+    
+    
+    class Meta: 
+        db_table = 'categories'
+        db_table_comment = 'This table for products` categories'
+        ordering = ['is_active', 'name', 'url_name',]
