7144dc4d7c8e0c2c6e5276a59d7f2126bfba005d
farhan <farhan.azizi.gh@gmail.com>
2026-07-14 17:50:37 +0330

    I added a few items to the category model and displayed them on the home page.

--- home/templatetags/base_tags.py
+++ home/templatetags/base_tags.py
@@ -1,14 +1,16 @@
 from django import template
-# from account
+from product.models import Category
+from django.db.models import Prefetch
 
 register = template.Library()
 
 
 @register.inclusion_tag('header_base.html')
 def header_base(request):
-    # user = 
+    categories = Category.objects.filter(is_active=True, parent__isnull=True, children_categories__is_active=True,).prefetch_related(Prefetch('children_categories', queryset=Category.objects.filter(is_active=True))).distinct()
     return {
         'request': request,
+        'categories': categories,
     } 
 
 
--- product/models.py
+++ product/models.py
@@ -6,8 +6,13 @@ from basic.base_model import BaseModel
 class Category(BaseModel):
     name = models.CharField(max_length=120, null=False, blank=False, verbose_name='نام دسته بندی')
     url_name = models.SlugField(max_length=120, null=False, blank=False, verbose_name='آدرس دسته بندی در url')
-    parent = models.ForeignKey(to='Category', on_delete=models.SET_NULL, null=True, blank=True, verbose_name='دسته بندی والد')
+    parent = models.ForeignKey(to='Category', on_delete=models.SET_NULL, null=True, blank=True, verbose_name='دسته بندی والد', related_name='children_categories')
     image = models.ImageField(upload_to='category/image', null=True, blank=True, verbose_name='عکس دسته بندی', help_text='فقط برای دسته بندی های والد استفاده می شود')
+    
+    
+    def __str__(self):
+        parent = '/ والد' if self.parent is None else ''
+        return f'{self.name} / {self.url_name} {parent}'
     
     
     class Meta: 
