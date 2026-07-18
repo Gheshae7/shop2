670780aa6c322e03baaa0c2e5d70d59386fc04b7
farhan <farhan.azizi.gh@gmail.com>
2026-07-18 12:15:13 +0330

    I created the brand model for the products.
    And I set the unique attribute to True for the name and url_name fields.
    And use command "python manage.py makemigrations" and "python manage.py migrate"
    And I also created the admin class for the brand model.

--- product/admin.py
+++ product/admin.py
@@ -1,10 +1,36 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Brand
 
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "url_name",
+        "is_active",
+        "updated_at",
+        "created_at",
+    )
+    list_filter = (
+        "created_at",
+        "updated_at",
+        "is_active",
+    )
+    list_editable = ("is_active",)
+    readonly_fields = (
+        "updated_at",
+        "created_at",
+    )
+    date_hierarchy = "created_at"
+    search_fields = (
+        "name",
+        "url_name",
+    )
+    
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
     list_display = (
         "name",
         "url_name",
--- product/models.py
+++ product/models.py
@@ -4,8 +4,10 @@ from basic.base_model import BaseModel
 
 
 class Category(BaseModel):
-    name = models.CharField(max_length=120, null=False, blank=False, verbose_name='نام دسته بندی')
-    url_name = models.SlugField(max_length=120, null=False, blank=False, verbose_name='آدرس دسته بندی در url')
+    """This class is for products categories."""
+    
+    name = models.CharField(max_length=120, null=False, blank=False, verbose_name='نام دسته بندی', unique=True)
+    url_name = models.SlugField(max_length=120, null=False, blank=False, verbose_name='آدرس دسته بندی در url', unique=True)
     parent = models.ForeignKey(to='Category', on_delete=models.SET_NULL, null=True, blank=True, verbose_name='دسته بندی والد', related_name='children_categories')
     image = models.ImageField(upload_to='category/image', null=True, blank=True, verbose_name='عکس دسته بندی', help_text='فقط برای دسته بندی های والد استفاده می شود')
     
@@ -18,4 +20,19 @@ class Category(BaseModel):
     class Meta: 
         db_table = 'categories'
         db_table_comment = 'This table for products` categories'
+        ordering = ['is_active', 'name', 'url_name',]
+        
+
+class Brand(BaseModel):
+    """This class is for products brands."""
+    
+    name = models.CharField(max_length=120, null=False, blank=False, verbose_name='نام برند', unique=True)
+    url_name = models.SlugField(max_length=120, null=False, blank=False, verbose_name='آدرس برند در url', unique=True)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        db_table = 'brands'
+        db_table_comment = 'This table for products` brands'
         ordering = ['is_active', 'name', 'url_name',]
