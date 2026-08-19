47c274ef7b64ed4e2d34edef638b571fe503cea4
farhan <farhan.azizi.gh@gmail.com>
2026-08-19 17:16:54 +0330

    I created NewsletterSubscriber
    and use command "python manage.py makemigrations" and "python manage.py
    migrate"

--- newsletter/models.py
+++ newsletter/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from basic.base_model import BaseModel
 # Create your models here.
+
+
+
+class NewsletterSubscriber(BaseModel):
+    """This class is for users who want to subscribe to the newsletter."""
+    
+    email = models.EmailField(unique=True, null=False, blank=False, verbose_name='ایمیل')
+    
+    def __str__(self):
+        return self.email
+    
+    class Meta:
+        db_table = 'news_letter_subscribers'
+        db_table_comment = 'This table is for users who want to subscribe to the newsletter.'
+        ordering = ['is_active', 'email']
