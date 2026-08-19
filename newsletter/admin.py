7f3b67dc654152b3e3b4102aa1ba6523aa93e273
farhan <farhan.azizi.gh@gmail.com>
2026-08-19 17:18:29 +0330

    I also set up the admin for the NewsletterSubscriber class.

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
-
+from .models import NewsletterSubscriber
 # Register your models here.
+
+
+@admin.register(NewsletterSubscriber)
+class NewsletterSubscriberAdmin(admin.ModelAdmin):
+    list_display = (
+        "email",
+        "is_active",
+        "updated_at",
+        "created_at",
+    )
+    list_filter = (
+        "created_at",
+        "updated_at",
+        "is_active",
+    )
+    readonly_fields = (
+        "updated_at",
+        "created_at",
+    )
+    date_hierarchy = "created_at"
+    search_fields = (
+        "is_active",
+        "email",
+    )
+    list_editable = (
+        "is_active",
+    )
+    
