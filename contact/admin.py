d33e728b26ffaef21fa8f3ce3c47da5290c65ac6
farhan <farhan.azizi.gh@gmail.com>
2026-08-22 19:38:00 +0330

    I created the `ContactUs` class and also set up the admin for it.

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import ContactUs
+
 
 # Register your models here.
+
+
+@admin.register(ContactUs)
+class ContactUsAdmin(admin.ModelAdmin):
+    list_display = (
+        "email",
+        "full_name",
+        "subject",
+        "is_active",
+        "is_read_by_admin",
+        "updated_at",
+        "created_at",
+    )
+    list_filter = (
+        "created_at",
+        "updated_at",
+        "is_active",
+    )
+    readonly_fields = (
+        "updated_at",
+        "created_at",
+        "subject",
+    )
+    date_hierarchy = "created_at"
+    search_fields = (
+        "is_active",
+        "full_name",
+        "subject",
+        "email",
+        "message",
+    )
+    list_editable = (
+        "is_active",
+        "is_read_by_admin",
+    )
+    
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from basic.base_model import BaseModel
 
-# Create your models here.
+
+class ContactUs(BaseModel):
+    """This class handles the interaction between the user and the site."""
+    
+    full_name = models.CharField(max_length=65, null=False, blank=False, verbose_name='نام و نام خانوادگی')
+    email = models.EmailField(max_length=100, null=False, blank=False, verbose_name='ایمیل')
+    phone = models.CharField(max_length=11, null=False, blank=False, verbose_name='شماره تلفن')
+    subject = models.CharField(max_length=100, null=False, blank=False, verbose_name='موضوع پیام')
+    message = models.TextField(null=False, blank=False, verbose_name='متن پیام')
+    is_read_by_admin = models.BooleanField(default=False, verbose_name='خوانده شده - نشده توسط ادمین')
+    
+    
+    def __str__(self):
+        return self.email
+    
+    
+    class Meta:
+        db_table = 'contacts_us'
+        db_table_comment = 'This table handles the interaction between the user and the site.'
+        ordering= ['is_active', 'email']
