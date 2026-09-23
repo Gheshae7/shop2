1133db7405351cb9eab0dfba22e31442c3067f54
farhan <farhan.azizi.gh@gmail.com>
2026-09-23 10:51:08 +0330

    I created BlogTag model for blog tags
    then use command "python manage.py makemigrations" & "python manage.py
    migrate"

--- blog/models.py
+++ blog/models.py
@@ -18,3 +18,19 @@ class BlogCategory(BaseModel):
         db_table_comment = 'This table is used for blog categories.'
         ordering = ['name']
         
+        
+class BlogTag(BaseModel):
+    """This class is for blog tags."""
+    
+    name = models.CharField(max_length=100, unique=True, blank=False, name=False, verbose_name='نام تگ')
+    
+    
+    def __str__(self):
+        return self.name
+    
+    
+    class Meta:
+        db_table = 'blog_tags'
+        db_table_comment = 'The tags for the blogs are listed in this table.'
+        ordering = ['name']
+    
