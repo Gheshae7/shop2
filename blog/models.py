a5e570cf4c046ae99797cf87f7c14c988f87df8b
farhan <farhan.azizi.gh@gmail.com>
2026-09-23 10:12:22 +0330

    I created BlogCategory model for blog categories.
    then use command "python manage.py makemigrations" & "python manage.py migrate"

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from basic.base_model import BaseModel
 
-# Create your models here.
+
+
+class BlogCategory(BaseModel):
+    """This class is used for blog categories."""
+    
+    name = models.CharField(max_length=120, null=False, blank=False, verbose_name='نام دسته بندی', unique=True)
+    url_name = models.SlugField(max_length=120, null=False, blank=False, verbose_name='آدرس دسته بندی در url', unique=True)
+    
+    def __str__(self):
+        return self.name
+    
+    
+    class Meta:
+        db_table = 'blog_categories'
+        db_table_comment = 'This table is used for blog categories.'
+        ordering = ['name']
+        
