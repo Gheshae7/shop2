b846f2017359c345760b92457f9b8a6bdf82151f
farhan <farhan.azizi.gh@gmail.com>
2026-09-23 10:56:01 +0330

    I created Blog Model.
    then use command "python manage.py makemigrations" & "python manage.py
    migrate"

--- blog/models.py
+++ blog/models.py
@@ -33,4 +33,17 @@ class BlogTag(BaseModel):
         db_table = 'blog_tags'
         db_table_comment = 'The tags for the blogs are listed in this table.'
         ordering = ['name']
+        
+
+class Blog(BaseModel):
+    """This class is for posts or blogs."""
     
+    name = models.CharField(max_length=450, null=False, blank=False, unique=True, verbose_name='نام مقاله')
+    category = models.ManyToManyField(BlogCategory, verbose_name='دسته بندی ها', related_name='blogs_c')
+    tag = models.ManyToManyField(BlogTag, verbose_name='تگ ها', related_name='blogs_t')
+    short_text = models.CharField(max_length=600, null=False, blank=False, verbose_name='توضیحات کوتاه')
+    summary = models.TextField(null=False, blank=False, verbose_name='خلاصه بلاگ')
+    reading_time = models.PositiveSmallIntegerField(verbose_name='مدت زمان مطالعه', null=False, blank=False)
+    image = models.ImageField(upload_to='blog/image/', verbose_name='عکس بلاگ', null=False, blank=False)
+
+
